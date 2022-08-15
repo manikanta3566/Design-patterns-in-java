8aefbfe8c1cc4f25cde3d9085c77f33400f05b57
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-08-15 18:44:41 +0530

    Update CricketBatCompany.java

--- FactoryPattern/CricketBatCompany.java
+++ FactoryPattern/CricketBatCompany.java
@@ -3,6 +3,10 @@ package com.practice.factorypattern;
 public abstract class CricketBatCompany {
     protected String name;
     protected String price;
+    public CricketBatCompany(String name,String price){
+        this.name=name;
+        this.price=price;
+    }
     public abstract void printBatCompany();
     public abstract void printBatPrice();
     }
