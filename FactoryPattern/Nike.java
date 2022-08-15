9c9f84c991882c3121ad7fe76f1ec7ba62d1f7e1
Manikanta M <59594162+manikanta3566@users.noreply.github.com>
2022-08-15 18:43:13 +0530

    Update Nike.java

--- FactoryPattern/Nike.java
+++ FactoryPattern/Nike.java
@@ -2,8 +2,7 @@ package com.practice.factorypattern;
 
 public class Nike extends CricketBatCompany{
     public Nike(String name, String price){
-        this.name=name;
-        this.price=price;
+        super(name,price);
     }
 
     @Override
